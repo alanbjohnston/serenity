310df5d9e16ccd4348cc6e7b5d716eed6e7f109f
Alan Johnston <alan.b.johnston@gmail.com>
2021-08-30 16:04:36 -0400

    changed WAV file no squelch

--- packet.sh
+++ packet.sh
@@ -91,8 +91,8 @@ else
 
 #  aplay -D hw:0,0 ~/Recordings/437.098_2021-08-28_10-41-34.wav &
 #  aplay -D hw:3,0,0 ~/Recordings/437.098_2021-08-28_10-41-34.wav &
-  aplay -D hw:0,0 WAV/SDRSharp_20210828_223743Z_437100196Hz_AF.wav &
-  aplay -D hw:${2:0:1},0,0 WAV/SDRSharp_20210828_223743Z_437100196Hz_AF.wav &
+  aplay -D hw:0,0 WAV/SDRSharp_20210830_200034Z_437097377Hz_AF.wav &
+  aplay -D hw:${2:0:1},0,0 WAV/SDRSharp_20210830_200034Z_437097377Hz_AF.wav &
 
   timeout 30 direwolf -c direwolf/direwolf-4800.conf -r 48000 -t 0
   
